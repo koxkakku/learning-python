2dd59e8a60e168731fdc3bcd79b2b5d6fa3f43d2
Sharad Kumar Dutta <sharad.k.dutta@gmail.com>
2023-10-27 11:41:37 +0530

    file operations in python
    simple file handling

---
+++ python-basics/fileops.py
@@ -0,0 +1,46 @@
+# to create a file
+# _file = open("C:\\Users\\skumard\\Documents\\file_output\\test_file.txt", 'x')
+# _file.close()
+
+
+# write to a file. this operation creates a new file if it doesnot exist
+# _write_file = open("C:\\Users\\skumard\\Documents\\file_output\\test_file.txt", 'a')
+# _write_file.write("\nMy name is python. \n I am high level programming language")
+# _write_file.flush()
+# _write_file.close()
+
+# overwrite contents of file. this operation creates a new file if it doesnot exist
+# _overwrite_file = open("C:\\Users\\skumard\\Documents\\file_output\\test_file.txt", 'w')
+# _overwrite_file.write("\nI am overwritten")
+# _overwrite_file.flush()
+# _overwrite_file.close()
+
+# read a file
+# _read_file = open("C:\\Users\\skumard\\Documents\\file_output\\test_file.txt", 'r')
+# print(_read_file.read())
+# _read_file.close()
+
+# read binary content
+# _read_bin_file = open("C:\\Users\\skumard\\Pictures\\Screenshot.png", 'rb')
+# bin_content = _read_bin_file.read()
+# _read_bin_file.close()
+
+# _create_new_file = open("\\Documents\\file_output\\newimg.png", 'x')
+# _create_new_file.close()
+
+ # write binary content. it creates a new file if it does not exist
+# _write_bin_content = open("C:\\Users\\skumard\\Documents\\file_output\\newimg.png", 'wb')
+# _write_bin_content.write(bin_content)
+# _write_bin_content.flush()
+# _write_bin_content.close()
+
+# copy contents of one file to another without new lines
+input_file = open("C:\\Users\\skumard\\Documents\\file_input\\input.txt")
+input_txt = input_file.read()
+input_file.close()
+print(len(input_txt.split()))
+output_txt =input_txt.replace("\n", ' ')
+output_file = open("C:\\Users\\skumard\Documents\\file_output\\output.txt", 'w')
+output_file.write(output_txt)
+output_file.flush()
+output_file.close()
